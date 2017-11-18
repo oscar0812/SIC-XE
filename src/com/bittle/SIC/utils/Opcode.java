326ddc7ff8994c2d460c208d67c90052a1cc2d3d
oscar0812 <oscar0812torres@gmail.com>
2017-11-17 22:33:20 -0600

    comparable and equal in order to search and sort

---
+++ src/com/bittle/SIC/utils/Opcode.java
@@ -0,0 +1,31 @@
+package com.bittle.SIC.utils;
+
+public class Opcode implements Comparable<Opcode>{
+    private String mnemonic;
+    private int hexCode;
+    public Opcode(String mnemonic, int hexCode){
+        this.mnemonic = mnemonic;
+        this.hexCode = hexCode;
+    }
+
+    public String getMnemonic() {
+        return mnemonic;
+    }
+
+    public int getHexCode() {
+        return hexCode;
+    }
+
+    @Override
+    public int compareTo(Opcode o) {
+        return o.getMnemonic().compareTo(getMnemonic());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj.getClass()!=getClass())
+            return false;
+        Opcode o = (Opcode)obj;
+        return o.getMnemonic().equals(getMnemonic());
+    }
+}
